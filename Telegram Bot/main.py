662924c1eed04535611a06234ca12d065741c852
kshyst <kiarash.sh1383@gmail.com>
2024-07-27 20:10:03 +0330

    json added

--- Telegram Bot/main.py
+++ Telegram Bot/main.py
@@ -1,8 +1,10 @@
 from telegram import Update
 import logging
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext, MessageHandler, Updater
+import json
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext, MessageHandler, Updater, \
+    filters
 
-token = '6556571128:AAEqJyqhPaDX1rXLNJqL8LhrsTLdrlAfqns'
+token = json.load(open("token.json"))["token"]
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -22,10 +24,19 @@ async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
     job = context.job
     await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
 
+
 async def set_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_message.chat_id
     due = float(context.args[0])
     context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+
+async def echo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text,
+        reply_to_message_id=update.effective_message.id
+    )
 
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
@@ -37,4 +48,5 @@ if __name__ == "__main__":
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("set", set_timer))
     application.add_handler(CommandHandler("ask", poll))
+    application.add_handler(MessageHandler(filters.TEXT, echo_handler))
     application.run_polling()
